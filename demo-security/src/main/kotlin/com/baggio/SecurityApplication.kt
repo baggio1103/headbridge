84ebbafebc90881f88a61a94937b00636d394e14
baggio1103 <valijanov1103@mail.ru>
2023-08-16 00:04:23 +0500

    Change java classes to Kotlin

---
+++ demo-security/src/main/kotlin/com/baggio/SecurityApplication.kt
@@ -0,0 +1,11 @@
+package com.baggio
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class SecurityApplication
+
+fun main() {
+    SpringApplication.run(SecurityApplication::class.java)
+}
